76a1cfbf6c9c584623154b923239325feb81359a
Hugo-Henrique7 <hugo.ccbh7@gmail.com>
2025-02-24 01:28:18 -0300

    rotas atualizadas

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__, static_folder='./static', template_folder='./templates')
 def home():
   return render_template("index.html")
 
+@app.route("/cadastro/")
+def cadastro():
+  return render_template("cadastro.html")
+
+@app.route("/login/")
+def login():
+  return render_template("login.html")
+
+@app.route("/baixar/")
+def baixar():
+  return render_template("baixar.html")
 
 
